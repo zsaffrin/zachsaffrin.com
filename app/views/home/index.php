966b7c599bb160c83970f7b4ff44198e89f1ba70
Zach Saffrin <awesomezach@gmail.com>
2015-05-20 21:56:33 -0400

    Layout cleanup

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,8 +1,6 @@
 
 <div class="clear pl4 mt4">
-    <div class="container">
-        <div class="clear bold">Hi, I&apos;m</div>
-        <div class="h1 strong caps">Zach</div>
-        <div class="clear">I make websites and stuff. Because I like to.</div>
-    </div>
+    <div class="clear bold">Hi, I&apos;m</div>
+    <div class="h1 strong caps">Zach</div>
+    <div class="clear">I make websites and stuff. Because I like to.</div>
 </div>
