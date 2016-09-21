c036e386add54814fed18d5008cb10aacdf2e458
Zach Saffrin <awesomezach@gmail.com>
2016-09-20 22:52:50 -0400

    Adds express server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express        = require('express'),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override'),
+    path           = require('path'),
+    app            = express();
+
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/'));
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve('./index.html'));
+});
+
+
+var server = app.listen(process.env.port || 80, function() {
+    var port = server.address().port;
+    console.log('Zach is listening on port %s. Yes... always listening...', port);
+});
