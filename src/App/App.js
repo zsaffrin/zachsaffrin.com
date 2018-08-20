ca59796087e1f8c0e8e39be490c57c637c6d740f
Zach Saffrin <zach@zachsaffrin.com>
2018-08-19 21:16:10 -0400

    Add Header component

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header/Header';
 
 const App = () => (
     <div className="App">
+        <Header />
+
         <h1>Hi, I&apos;m Zach</h1>
         <p>This is version 7.0.0 of my website</p>
     </div>
