10b07db8212f09c30e1216d8f9e9942f8a4c0764
Zach Saffrin <zach@zachsaffrin.com>
2018-08-31 22:41:17 -0400

    Add Client Work section, use data files for page content

--- src/components/App/Content/Home/Home.js
+++ src/components/App/Content/Home/Home.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import './Home.css';
 import ItemList from './ItemList/ItemList';
+import projectItems from '../../../../data/projectItems.json';
+import clientItems from '../../../../data/clientItems.json';
 
-const Home = () => {
-    const projectItems = [
-        {
-            title: 'OverEasy',
-            subtitle: 'A deliciously flexible OOCSS framework',
-            link: 'http://overeasycss.com',
-        },
-    ];
+const Home = () => (
+    <div className="Home">
+        <section className="hero">
+            <div className="subtitle top">
+                <div className="content">Hi, I&apos;m</div>
+            </div>
+            <div className="title">
+                Zach
+            </div>
+            <div className="subtitle">
+                I like building stuff with code
+            </div>
+        </section>
 
-    return (
-        <div className="Home">
-            <section className="hero">
-                <div className="subtitle top">
-                    <div className="content">Hi, I&apos;m</div>
-                </div>
-                <div className="title">
-                    Zach
-                </div>
-                <div className="subtitle">
-                    I like building stuff with code
-                </div>
-            </section>
+        <section>
+            <h2>Projects</h2>
+            <ItemList items={projectItems} />
+        </section>
 
-            <section>
-                <h2>Projects</h2>
-                <ItemList items={projectItems} />
-            </section>
-        </div>
-    );
-};
+        <section>
+            <h2>Client Work</h2>
+            <ItemList items={clientItems} />
+        </section>
+    </div>
+);
 
 export default Home;
